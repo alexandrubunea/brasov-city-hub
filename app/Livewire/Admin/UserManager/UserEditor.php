8963b8b8171df982b5b386b1629a0aa7a3779131
Alexandru Bunea <alexandrubunea03@gmail.com>
2024-12-27 16:47:11 +0200

    feat: add files for user manager page

---
+++ app/Livewire/Admin/UserManager/UserEditor.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Admin\UserManager;
+
+use Livewire\Component;
+
+class UserEditor extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.user-manager.user-editor');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'index');
 
-Route::view('roles', 'admin.roles-manager')->name('roles.view');
+Route::view('manage/roles', 'admin.roles-manager')->name('roles.view');
+Route::view('manage/users', 'admin.users-manager')->name('users.view');
