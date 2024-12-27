47b91c470432b884d533393bf6ac2353301f95a4
Alexandru Bunea <alexandrubunea03@gmail.com>
2024-12-27 04:07:12 +0200

    fix: improve navbar and remove flowbite

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,3 @@
 import './bootstrap';
-import 'flowbite';
 import Swal from 'sweetalert2';
 window.Swal = Swal;
--- resources/views/livewire/navigation-bar.blade.php
+++ resources/views/livewire/navigation-bar.blade.php
@@ -1,17 +1,28 @@
 <div>
-    <nav class="bg-slate-900 text-zinc-200 p-5">
+    <nav class="bg-slate-900 text-zinc-200 p-5" x-data="{
+        open: window.innerWidth >= 1024,
+        init() {
+            window.addEventListener('resize', () => {
+                this.open = window.innerWidth >= 1024 ? true : this.open
+            })
+        }
+    }">
         <div class="flex flex-col lg:flex-row lg:flex-wrap lg:justify-between lg:items-center">
             <div class="flex flex-wrap justify-between">
                 <a href="{{ URL('/') }}">
                     <img class="w-32 float-left" src="{{ asset('storage/images/logo.png') }}" alt="Brasov City Hub Logo">
                 </a>
-                <button data-collapse-toggle="navbar-collapse" type="button"
-                    class="lg:hidden border-4 border-zinc-200 rounded-lg bg-indigo-700 text-4xl p-3 hover:text-indigo-700 hover:border-indigo-700 hover:bg-zinc-200 transition-all duration-500"
-                    aria-controls="navbar-collapse" aria-expanded="false">
+                <button type="button" @click="open = !open"
+                    class="lg:hidden border-4 border-zinc-200 rounded-lg bg-indigo-700 text-4xl p-3 hover:text-indigo-700 hover:border-indigo-700 hover:bg-zinc-200 transition-all duration-500">
                     <i class="fa-solid fa-bars"></i>
                 </button>
             </div>
-            <div id="navbar-collapse" class="hidden lg:block">
+            <div x-show="open" x-collapse x-cloak x-transition:enter="transition ease-out duration-200"
+                x-transition:enter-start="opacity-0 transform scale-95"
+                x-transition:enter-end="opacity-100 transform scale-100"
+                x-transition:leave="transition ease-in duration-75"
+                x-transition:leave-start="opacity-100 transform scale-100"
+                x-transition:leave-end="opacity-0 transform scale-95">
                 <ul
                     class="flex flex-col gap-2 mt-5 p-5 bg-slate-800 rounded-lg lg:mt-0 lg:p-0 lg:bg-transparent lg:rounded-none lg:flex-row lg:justify-between lg:gap-3">
                     @foreach ($tabs as $key => $tab)
