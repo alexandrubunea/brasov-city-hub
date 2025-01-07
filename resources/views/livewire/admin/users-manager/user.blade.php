451c39a40a783c9c02c8cdd26741b6437cbdd70d
Bunea Alexandru <alexandrubunea03@gmail.com>
2025-01-07 22:07:36 +0200

    fix: improve layout for banned user display in user manager

--- resources/views/livewire/admin/users-manager/user.blade.php
+++ resources/views/livewire/admin/users-manager/user.blade.php
@@ -1,10 +1,13 @@
 <div class="bg-indigo-700 hover:bg-indigo-800 transition-colors duration-1000 p-5 rounded-md hover:cursor-pointer"
     wire:click="clickUser">
-    <h1 class="font-bold text-lg">{{ $first_name . ' ' . $last_name }}
+    <div class="flex flex-auto gap-5 justify-between">
+        <h1 class="font-bold text-lg">{{ $first_name . ' ' . $last_name }} </h1>
         @if ($banned)
-            <span class="uppercase font-bold p-2 bg-red-500 rounded text-xs ml-3">banned</span>
+            <div class="my-auto items-center">
+                <span class="uppercase font-bold p-1 bg-red-500 rounded text-xs">banned</span>
+            </div>
         @endif
-    </h1>
+    </div>
     <p class="font-light text-sm">
         Username: <span class="text-red-500">{{ $username }}</span> <br>
         E-mail: <span class="text-red-500">{{ $email }}</span> <br>
